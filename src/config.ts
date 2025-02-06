0acea4ac22fb2693338a72bcf71d4badd1602194
MiaoLisrity <124774171+MiaoLisrity@users.noreply.github.com>
2025-02-06 15:50:51 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
 export let PROXIES: Proxy[] = [
     {
-        domain: "arden.moe",
-        url: "https://pixiv.net/",
+        domain: "search.arden.moe",
+        url: "https://google.com/",
         enable: true
     }
 ];
