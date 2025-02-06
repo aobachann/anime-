7f00eedc48553368c3437082a91ba7dad5de2239
MiaoLisrity <124774171+MiaoLisrity@users.noreply.github.com>
2025-02-06 15:26:16 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 import type {Proxy} from './util/model';
 
-export let PORT = 377; // 端口
+export let PORT = 443; // 端口
 
 export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
 export let PROXIES: Proxy[] = [
     {
-        domain: "arden.moe",
+        domain: "https://arden.moe",
         url: "https://blog.wtf.day/",
         enable: true
     }
